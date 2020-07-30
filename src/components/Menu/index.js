9e7386311d9d943b6dda107e72fd9d804ee4d650
willianlemes <willianlemes94@gmail.com>
2020-07-30 14:55:26 -0300

    Criação dos cadastros de vídeos e categoria

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,18 +2,21 @@ import React from 'react';
 import logo from '../../assets/img/logo.png';
 import Button from '../Button';
 import './menu.css';
+import { Link } from 'react-router-dom';
 
 function Menu() {
   return (
     <nav className="Menu">
-      <img
-        className="Logo"
-        src={logo}
-        title="esse aqui é um title"
-        alt="esse aqui é um alt"
-      />
+      <Link to="/">
+        <img
+          className="Logo"
+          src={logo}
+          title="esse aqui é um title"
+          alt="esse aqui é um alt"
+        />
+      </Link>
 
-      <Button href="/" className="ButtonLink">
+      <Button as={Link} to="/cadastro/video" className="ButtonLink">
         Novo Vídeo
       </Button>
     </nav>
---
+++ src/pages/Pagina404/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Pagina404() {
+  return <h1>Página 404</h1>;
+}
+
+export default Pagina404;
